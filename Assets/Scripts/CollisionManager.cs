1768577e973d41794580cfc120b15f13e6961185
Ailton-exe <ailton.ramos23b@udgvirtual.udg.mx>
2025-09-17 12:27:54 -0600

    2.6. El flujo de la interfaz
    Se agregaron coliciones y fue el final de la unidad 2

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -1,9 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision)
+    Animator bubble_Animator;
+    GameObject bubble;
+    GameObject[] bubbles;
+
+    void Awake()
     {
-        Debug.Log("Collision detected with: " + collision.gameObject.name);
+        bubbles = GameObject.FindGameObjectsWithTag("Bubbles");
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        foreach (GameObject bubble in bubbles)
+        {
+            bubble_Animator = bubble.GetComponent<Animator>();
+            bubble_Animator.SetBool("isBubbling", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        foreach (GameObject bubble in bubbles)
+        {
+            bubble_Animator = bubble.GetComponent<Animator>();
+            bubble_Animator.SetBool("isBubbling", false);
+        }
     }
 }
